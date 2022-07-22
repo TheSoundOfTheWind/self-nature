c7213f0c3cb4c2c2b7ad422fcb143e3154dcca27
TheSoundOfTheWind <18270949493@163.com>
2022-07-22 17:37:57 +0800

    add file

---
+++ 物性/形式科学/计算机科学/代码/软件工程/开发工具/Makefile/GenericMakefile/c/hello.c
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  // suppress warnings
+  (void)argc; (void)argv;
+
+  printf("Hello World!\n");
+  return 0;
+}
+=======
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  // suppress warnings
+  (void)argc; (void)argv;
+
+  printf("Hello World!\n");
+  return 0;
+}
+>>>>>>> 1d751b2d5d0f314aae5ee85cadda02a076b13d0f
---
+++ 物性/形式科学/计算机科学/代码/软件工程/开发工具/Makefile/GenericMakefile/cpp/hello.cpp
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  // suppress warnings
+  (void)argc; (void)argv;
+
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
+=======
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  // suppress warnings
+  (void)argc; (void)argv;
+
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
+>>>>>>> 1d751b2d5d0f314aae5ee85cadda02a076b13d0f
